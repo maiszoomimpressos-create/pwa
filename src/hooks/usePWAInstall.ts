4df1d9b8b78e8165a0c19991c74e7501bf54a78a
[dyad] <maiszoomimpressos@gmail.com>
2025-11-16 02:08:26 -0300

    [dyad] Fixing hook import error - wrote 1 file(s)

--- src/hooks/usePWAInstall.ts
+++ src/hooks/usePWAInstall.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // Tipo para o evento de instalação do PWA
 interface BeforeInstallPromptEvent extends Event {
